05151df39976d7523b9d238d1cf91f4c9fcfa3e0
yogasab <baskoroyoga40@gmail.com>
2021-11-09 22:30:55 +0700

    Add API Routes to Web

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+Route::get('contact', 'API\Contact\ContactController@query');
+Route::resource('contacts', 'API\Contact\ContactController');
+Route::resource('labels', 'API\Label\LabelController');
+Route::resource('contact-label', 'API\ContactModel\ContactLabelController')->except(['store']);
+Route::post('/contact-label/{contact}/{label}', 'API\ContactModel\ContactLabelController@storeContactLabel')->name('contact-label');
